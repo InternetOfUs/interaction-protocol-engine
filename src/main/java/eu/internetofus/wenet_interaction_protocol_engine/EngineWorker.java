4ae3ab73b15ce9c7d3669339903d9de8dbe87f36
Bruno Rosell i Gui (UDT-IA, IIIA-CSIC) <rosell@localhost>
2020-05-15 19:36:58 +0200

    Basic protocol (Issue #1)

--- src/main/java/eu/internetofus/wenet_interaction_protocol_engine/EngineWorker.java
+++ src/main/java/eu/internetofus/wenet_interaction_protocol_engine/EngineWorker.java
@@ -28,9 +28,15 @@ package eu.internetofus.wenet_interaction_protocol_engine;
 
 import org.tinylog.Logger;
 
+import eu.internetofus.common.TimeManager;
 import eu.internetofus.common.components.Model;
 import eu.internetofus.common.components.interaction_protocol_engine.InteractionProtocolMessage;
-import eu.internetofus.common.components.service.TextualMessage;
+import eu.internetofus.common.components.service.App;
+import eu.internetofus.common.components.service.MessageFromUserNotification;
+import eu.internetofus.common.components.service.TaskConcludedNotification;
+import eu.internetofus.common.components.service.TaskProposalNotification;
+import eu.internetofus.common.components.service.TaskSelectionNotification;
+import eu.internetofus.common.components.service.TaskVolunteerNotification;
 import eu.internetofus.common.components.service.WeNetServiceApiService;
 import eu.internetofus.common.vertx.Worker;
 import io.vertx.core.AbstractVerticle;
@@ -120,54 +126,165 @@ public class EngineWorker extends AbstractVerticle implements Handler<Message<Js
 
 		EngineEnvironment.create(this.vertx, message).onComplete(creation -> {
 
-			WeNetServiceApiService.createProxy(this.vertx).retrieveJsonArrayAppUserIds(message.appId, retrieve -> {
+			try {
 
-				if (retrieve.failed()) {
+				final EngineEnvironment env = creation.result();
+				if (message.content instanceof JsonObject) {
 
-					Logger.debug("No found users from the APP");
+					final JsonObject content = (JsonObject) message.content;
+					final String action = content.getString("action");
+					final WebClientOptions options = new WebClientOptions();
+					final WebClient client = WebClient.create(this.vertx, options);
+					if ("TaskCreation".equalsIgnoreCase(action)) {
 
-				} else {
+						final TaskProposalNotification notification = new TaskProposalNotification();
+						notification.taskId = env.task.id;
+						notification.title = "Can you help?";
+						notification.text = env.task.goal.name;
+						notification.title = "Can you help?";
+						this.sendToAllAppUsers(env.app, client, notification, env.task.requesterId);
 
-					try {
+					} else if ("volunteerForTask".equalsIgnoreCase(action)) {
 
-						final JsonArray users = retrieve.result();
-						final EngineEnvironment env = creation.result();
-						final WebClientOptions options = new WebClientOptions();
-						final WebClient client = WebClient.create(this.vertx, options);
-						final TextualMessage textualMessage = new TextualMessage();
-						textualMessage.title = "Dummy protocol";
-						textualMessage.text = String.valueOf(message.content);
-						for (int i = 0; i < users.size(); i++) {
+						final String volunteerId = content.getJsonObject("attributes", new JsonObject()).getString("volunteerId",
+								"0");
+						if (env.task.deadlineTs < TimeManager.now()) {
 
-							final String userId = users.getString(i);
-							if (message.senderId == null || !message.senderId.equals(userId)) {
+							final TaskVolunteerNotification notification = new TaskVolunteerNotification();
+							notification.recipientId = env.task.requesterId;
+							notification.taskId = env.task.id;
+							notification.title = "Found volunteer";
+							notification.text = "An user want to help you.";
+							notification.volunteerId = volunteerId;
+							this.sendTo(env.app, client, notification);
 
-								textualMessage.recipientId = userId;
-								final JsonObject body = textualMessage.toJsonObject();
-								client.postAbs(env.app.messageCallbackUrl).sendJsonObject(body, send -> {
-
-									if (send.failed()) {
-
-										Logger.error(send.cause(), "Can not notify to {} about {}.", userId, message);
-
-									} else {
-
-										Logger.debug("Sent {} to {}", body, env.app.messageCallbackUrl);
-									}
-
-								});
-							}
+						} else {
+							// too late
+							final MessageFromUserNotification notification = new MessageFromUserNotification();
+							notification.recipientId = env.task.requesterId;
+							notification.taskId = env.task.id;
+							notification.title = "Deadline reached";
+							notification.text = "It's too late to volunteer.";
+							notification.senderId = env.task.requesterId;
+							this.sendTo(env.app, client, notification);
 
 						}
 
-					} catch (final Throwable throwable) {
+					} else if ("refuseTask".equalsIgnoreCase(action)) {
+						// nothing to do
+						Logger.debug("Ignored {}, because any action is necessary", message);
 
-						Logger.error(throwable, "Can not process {}", message);
+					} else if ("acceptVolunteer".equalsIgnoreCase(action)) {
+
+						final String volunteerId = content.getJsonObject("attributes", new JsonObject()).getString("volunteerId",
+								"0");
+						final TaskSelectionNotification notification = new TaskSelectionNotification();
+						notification.recipientId = volunteerId;
+						notification.taskId = env.task.id;
+						notification.title = "Help not needed";
+						notification.text = "You has been selected to do r help is not necessary to do the task.";
+						notification.outcome = TaskSelectionNotification.Outcome.accepted;
+						this.sendTo(env.app, client, notification);
+
+					} else if ("refuseVolunteer".equalsIgnoreCase(action)) {
+
+						final String volunteerId = content.getJsonObject("attributes", new JsonObject()).getString("volunteerId",
+								"0");
+						final TaskSelectionNotification notification = new TaskSelectionNotification();
+						notification.recipientId = volunteerId;
+						notification.taskId = env.task.id;
+						notification.title = "Help not needed";
+						notification.text = "Your help is not necessary to do the task.";
+						notification.outcome = TaskSelectionNotification.Outcome.refused;
+						this.sendTo(env.app, client, notification);
+
+					} else if ("taskCompleted".equalsIgnoreCase(action)) {
+
+						final String outcome = content.getJsonObject("attributes", new JsonObject()).getString("outcome",
+								"cancelled");
+						final TaskConcludedNotification notification = new TaskConcludedNotification();
+						notification.taskId = env.task.id;
+						notification.title = "Help not needed";
+						notification.text = "Your help is not necessary to do the task.";
+						notification.outcome = TaskConcludedNotification.Outcome.valueOf(outcome);
+						this.sendToAllAppUsers(env.app, client, notification, env.task.requesterId);
+
+					} else {
+
+						Logger.error("Unexpected action {}", message);
+					}
+
+				} else {
+
+					Logger.error("Unexpected {}", message);
+				}
+
+			} catch (final Throwable throwable) {
+
+				Logger.error(throwable, "Can not process {}", message);
+			}
+
+		});
+
+	}
+
+	/**
+	 * Called when want to notify all users about a notification.
+	 *
+	 * @param app          application to notify all the users.
+	 * @param client       to use.
+	 * @param notification to send to the users.
+	 * @param senderId     identifier of the user that send the notification.
+	 */
+	private void sendToAllAppUsers(App app, WebClient client,
+			eu.internetofus.common.components.service.Message notification, String senderId) {
+
+		WeNetServiceApiService.createProxy(this.vertx).retrieveJsonArrayAppUserIds(app.appId, retrieve -> {
+
+			if (retrieve.failed()) {
+
+				Logger.debug("No found users from the APP to send {}", notification);
+
+			} else {
+
+				final JsonArray users = retrieve.result();
+				for (int i = 0; i < users.size(); i++) {
+
+					final String userId = users.getString(i);
+					if (!senderId.equals(userId)) {
+
+						notification.recipientId = userId;
+						this.sendTo(app, client, notification);
 					}
 
 				}
 
-			});
+			}
+		});
+
+	}
+
+	/**
+	 * Called when want to send a notification to an application.
+	 *
+	 * @param app          application to notify.
+	 * @param client       to use.
+	 * @param notification to send to the application.
+	 */
+	private void sendTo(App app, WebClient client, eu.internetofus.common.components.service.Message notification) {
+
+		final JsonObject body = notification.toJsonObject();
+		client.postAbs(app.messageCallbackUrl).sendJsonObject(body, send -> {
+
+			if (send.failed()) {
+
+				Logger.error(send.cause(), "Can not notify about  {} to {}.", body, app);
+
+			} else {
+
+				Logger.debug("Sent {}  to {}", body, app);
+			}
+
 		});
 
 	}
