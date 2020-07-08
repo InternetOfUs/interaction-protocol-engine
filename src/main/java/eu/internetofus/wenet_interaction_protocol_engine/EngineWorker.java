a75bbd1b0c92f82e7691f0ce5e1de420c4719716
Bruno Rosell i Gui (UDT-IA, IIIA-CSIC) <rosell@localhost>
2020-07-08 12:23:47 +0200

    Empty title

--- src/main/java/eu/internetofus/wenet_interaction_protocol_engine/EngineWorker.java
+++ src/main/java/eu/internetofus/wenet_interaction_protocol_engine/EngineWorker.java
@@ -734,7 +734,7 @@ public class EngineWorker extends AbstractVerticle implements Handler<io.vertx.c
     final WebClient client = WebClient.create(this.vertx, options);
 
     final TextualMessage notification = new TextualMessage();
-    notification.title = "There is a system message for you";
+    notification.title = "";
     notification.recipientId = incentive.UserId;
     if (incentive.Message != null) {
 
