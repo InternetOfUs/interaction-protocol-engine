099456d0aea39dbf8e47328432e4d939c45d132f
Bruno Rosell i Gui (UDT-IA, IIIA-CSIC) <rosell@localhost>
2020-07-08 11:29:05 +0200

    Change title message.

--- src/main/java/eu/internetofus/wenet_interaction_protocol_engine/EngineWorker.java
+++ src/main/java/eu/internetofus/wenet_interaction_protocol_engine/EngineWorker.java
@@ -734,7 +734,7 @@ public class EngineWorker extends AbstractVerticle implements Handler<io.vertx.c
     final WebClient client = WebClient.create(this.vertx, options);
 
     final TextualMessage notification = new TextualMessage();
-    notification.title = "Incentive";
+    notification.title = "There is a system message for you";
     notification.recipientId = incentive.UserId;
     if (incentive.Message != null) {
 
