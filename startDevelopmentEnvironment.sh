e5871c2f5d5f869de0aed0efe193d97b21c6b46e
Bruno Rosell i Gui (UDT-IA, IIIA-CSIC) <rosell@localhost>
2020-10-22 17:51:59 +0200

    Rename docker image.

--- startDevelopmentEnvironment.sh
+++ startDevelopmentEnvironment.sh
@@ -2,6 +2,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR >/dev/null
 docker-compose -p wenet_interaction_protocol_engine_services_dev -f src/dev/docker/docker-compose.yml up --remove-orphans -d
-DOCKER_BUILDKIT=1 docker build -f src/dev/docker/Dockerfile -t wenet/interaction-protocol-engine:dev .
-docker run --name wenet_interaction_protocol_engine_dev -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/.m2/repository:/root/.m2/repository -p 5005:5005 -v ${PWD}:/app -it wenet/interaction-protocol-engine:dev /bin/bash
+DOCKER_BUILDKIT=1 docker build -f src/dev/docker/Dockerfile -t internetofus/interaction-protocol-engine:dev .
+docker run --name wenet_interaction_protocol_engine_dev -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/.m2/repository:/root/.m2/repository -p 5005:5005 -v ${PWD}:/app -it internetofus/interaction-protocol-engine:dev /bin/bash
 popd >/dev/null
