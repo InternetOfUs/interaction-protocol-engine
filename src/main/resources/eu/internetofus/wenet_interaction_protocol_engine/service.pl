1fe847a51a62514b9fa4afaf129c535f992a9778
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-03-11 18:43:50 +0100

    Fix send callback to app.

--- src/main/resources/eu/internetofus/wenet_interaction_protocol_engine/service.pl
+++ src/main/resources/eu/internetofus/wenet_interaction_protocol_engine/service.pl
@@ -112,8 +112,8 @@ wenet_service_get_app_users(Users,Id) :-
 %	@param Callback to post.
 %	@param App json application to post the message.
 %
-wenet_service_post_callback(Response,Callback,json(App)) :-
-	member(messageCallbackUrl=Url,App),
+wenet_service_post_callback(Response,Callback,App) :-
+	wenet_message_callback_url_of_app(Url,App),
 	wenet_service_post_callback(Response,Callback,Url)
 	.
 
@@ -126,7 +126,6 @@ wenet_service_post_callback(Response,Callback,json(App)) :-
 %	@param Url to post the callback message.
 %
 wenet_service_post_callback(Response,Callback,Url) :-
-	string(Url),
 	wenet_post_json_to_url(Response,Url,Callback,[])
 	.
 
