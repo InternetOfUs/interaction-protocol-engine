86c088cf9b5ef1c45c4b71a86bf68500a8f2719a
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-08-19 14:27:31 +0200

    Auto stop environment when exit

--- stopDevelopmentEnvironment.sh
+++ stopDevelopmentEnvironment.sh
@@ -5,7 +5,9 @@ else
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 	pushd $DIR >/dev/null
 	docker-compose -p wenet_interaction_protocol_engine_services_dev -f src/dev/docker/docker-compose.yml down --remove-orphans
-	docker stop wenet_interaction_protocol_engine_dev
-	docker rm wenet_interaction_protocol_engine_dev
+	if [ "$(docker container ls |grep wenet_interaction_protocol_engine_dev |wc -l)" -gt "0" ]
+	then
+		docker stop wenet_interaction_protocol_engine_dev
+	fi
 	popd >/dev/null
 fi
