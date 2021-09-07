ace0ad465563e561ba847a02f006fa5e06f94f88
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-09-07 15:47:30 +0200

    Fix directory with spaces

--- startDevelopmentEnvironment.sh
+++ startDevelopmentEnvironment.sh
@@ -3,10 +3,10 @@ if [ -f /.dockerenv ]; then
    echo "You can not start the development environment inside a docker container"
 else
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-	pushd $DIR >/dev/null
+	pushd "$DIR" >/dev/null
 	docker-compose -p wenet_interaction_protocol_engine_services_dev -f src/dev/docker/docker-compose.yml up --remove-orphans -d
 	DOCKER_BUILDKIT=1 docker build -f src/dev/docker/Dockerfile -t internetofus/interaction-protocol-engine:dev .
-	docker run --rm --name wenet_interaction_protocol_engine_dev -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/.m2/repository:/root/.m2/repository -p 5005:5005 -v ${PWD}:/app -it internetofus/interaction-protocol-engine:dev /bin/bash
+	docker run --rm --name wenet_interaction_protocol_engine_dev -v /var/run/docker.sock:/var/run/docker.sock -v "${HOME}/.m2/repository":/root/.m2/repository -p 5005:5005 -v "${PWD}":/app -it internetofus/interaction-protocol-engine:dev /bin/bash
 	./stopDevelopmentEnvironment.sh
 	popd >/dev/null
 fi
