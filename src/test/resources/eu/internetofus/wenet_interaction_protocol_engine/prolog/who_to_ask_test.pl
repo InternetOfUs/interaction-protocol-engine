0d5ef09e4d694eac30ee0a28d3c1477492dc3db4
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-10-29 17:07:44 +0200

    Fix attributes for domains and believe and values.

--- src/test/resources/eu/internetofus/wenet_interaction_protocol_engine/prolog/who_to_ask_test.pl
+++ src/test/resources/eu/internetofus/wenet_interaction_protocol_engine/prolog/who_to_ask_test.pl
@@ -7,7 +7,8 @@
 	users_by_domain_interest/2,
 	get_profile_attribues_by_beliefs_and_values/1,
 	get_profile_attribues_by_domain_interest/1,
-	attributes_by_domain/2
+	attributes_by_domain/2,
+	attributes_similars_to_question/1
 	.
 	
 who_to_ask(Users) :-
@@ -83,7 +84,7 @@ users_by_beliefs_and_values(BeliefsAndValuesUsers,Users) :-
 	),
 	put_task_state_attribute('beliefsAndValuesUsers',BeliefsAndValuesUsers).
 	
-get_profile_attribues_by_beliefs_and_values(['gender','nationality','occupation']).
+get_profile_attribues_by_beliefs_and_values(['meanings.excitement','meanings.promotion','meanings.existence','meanings.suprapersonal','meanings.interactive','meanings.normative','meanings.extraversion','meanings.agreeableness','meanings.consientiousness','meanings.neuroticism','meanings.openness']).
 	
 users_by_domain_interest(DomainInterestUsers,Users) :-
 	(
@@ -104,19 +105,36 @@ users_by_domain_interest(DomainInterestUsers,Users) :-
 	
 get_profile_attribues_by_domain_interest(Attributes) :-
 	get_task_attribute_value(Domain,'domain'),
-	attributes_by_domain(Attributes,Domain).
+	attributes_by_domain(Attributes,Domain),
+	!,
+	retractall(get_profile_attribues_by_domain_interest(_)),
+	asserta(get_profile_attribues_by_domain_interest(Attributes))
+	.
 	
-attributes_by_domain('studying_career',['gender','nationality','occupation']).
-attributes_by_domain('local_university',['gender','nationality','occupation']).
-attributes_by_domain('local_things',['gender','nationality','occupation']).
-attributes_by_domain('physical_activity',['gender','nationality','occupation']).
-attributes_by_domain('cultural_interests',['gender','nationality','occupation']).
-attributes_by_domain('food_and_cooking',['gender','nationality','occupation']).
-attributes_by_domain('cinema_theatre',['gender','nationality','occupation']).
-attributes_by_domain('music',['gender','nationality','occupation']).
-attributes_by_domain('arts_and_crafts',['gender','nationality','occupation']).
-attributes_by_domain('life_ponders',['gender','nationality','occupation']).
+attributes_by_domain('studying_career',['competences.u_active','competences.u_read','competences.u_essay','competences.u_org','competences.u_balance','competences.u_assess','competences.u_theory','competences.u_pract']).
+attributes_by_domain('local_university',['competences.c_locfac','competences.c_accom']).
+attributes_by_domain('local_things',['competences.c_accom']).
+attributes_by_domain('physical_activity',['competences.c_team_sp','competences.c_ind_sp','competences.c_watch_sp']).
+attributes_by_domain('cultural_interests',['competences.c_lit','competences.c_creatlit','competences.c_perf_art','competences.c_musgall']).
+attributes_by_domain('food_and_cooking',['competences.c_food','competences.c_eating']).
+attributes_by_domain('cinema_theatre',['competences.c_plays','competences.c_perf_plays']).
+attributes_by_domain('music',['competences.c_app_mus','competences.c_perf_mus']).
+attributes_by_domain('arts_and_crafts',['competences.c_perf_art','competences.c_musgall']).
+attributes_by_domain('life_ponders',Attributes) :-
+	attributes_similars_to_question(Attributes)
+	.
 attributes_by_domain('varia_misc',Attributes) :-
-	get_task_goal_name(Question),
-	my_profile_attributes_similars_to(Attributes,Question,0.4)
+	attributes_similars_to_question(Attributes)
+	.
+
+attributes_similars_to_question(Attributes) :-
+	(
+		get_task_goal_name(Question),
+		my_profile_attributes_similars_to(Attributes,Question,0.4)
+		-> true
+		; Attributes = []
+	),
+	!,
+	retractall(attributes_similars_to_question(_)),
+	asserta(attributes_similars_to_question(Attributes))
 	.
